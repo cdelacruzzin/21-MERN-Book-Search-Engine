d87041a03a60054add4b147de6bfa130bfea9f84
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-13 16:17:49 -0400

    changed the auth.js to fit with graphql and Apollo server

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -15,25 +15,27 @@ module.exports = {
       token = token.split(' ').pop().trim();
     }
 
+    //if no token found, the middleware returns the request object unchanged(any resolver using this request is not authenticated)
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
     try {
+      //if the token is verified, the "verify" function will return the payload token
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      req.user = data;  //the decoded user data from the token is added to the request object
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    return req; //return the request object so it can be passed to the resolver as "context"
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  //function to generate a JWT token
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id }; //stores the data(payload) that we want to store the token
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });  //the "sign" method creates and returns a new JWT, encoding the payload with the secret
   },
 };
