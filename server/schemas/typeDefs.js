29c9a74e8ecbd7d61ee34e27df6141f15056420a
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-13 16:57:51 -0400

    creeated type mutations and queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -25,10 +25,17 @@ type Auth {
 }
 
 type Query {
-
+    me: User
+    users: [User]
+    singleUser(userId: ID!): User
 }
 type mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    removeBook(bookId: bookId!): User
 
+
+    #still need to save a book
 }
 
 
