48f000fd9053d96ee1b64f5f0b6e15a2da405d3b
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-13 17:02:43 -0400

    imported apollo client and graphql to client package

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+
+
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
 
 function App() {
   return (
