e89a3c26ab2a64c4cef2f5437a57bd2314508335
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-13 16:44:37 -0400

    creeated gql model types

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String!
+    authors: String
+    description: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+
+}
+type mutation {
+
+}
+
+
+
+
+`
