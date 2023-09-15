292bb4fd1c670c4fb762d7898149b52126d3d1a8
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-15 15:33:50 -0400

    removed the API call to login in login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -26,18 +25,9 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      
+    } catch (error) {
+      console.log(error)
     }
 
     setUserFormData({
